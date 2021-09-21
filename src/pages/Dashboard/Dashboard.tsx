2aa193c4677ec461f8a94fb5d1ccff4a17c426d6
Daniel Garcia Alanis <daniel@dagasoft.com>
2021-09-21 13:27:19 -0400

    Added search functionality

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -4,13 +4,27 @@ import { getCurrencyTotals } from '../../usecases/getCurrencyTotals';
 import CurrencyTable from '../components/CurrencyTable/CurrencyTable';
 
 interface State {
+    loading: boolean;
+    search: string;
+    error: string;
     currencies: Currency[];
+    searchedCurrencies: Currency[];
 }
 
 class Dashboard extends React.Component<unknown, State> {
+    public constructor(props: unknown) {
+        super(props);
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
     // State of the component
     public readonly state: Readonly<State> = {
+        loading: true,
+        search: '',
+        error: '',
         currencies: [],
+        searchedCurrencies: [],
     };
 
     /**
@@ -20,29 +34,45 @@ class Dashboard extends React.Component<unknown, State> {
     async componentDidMount(): Promise<void> {
         // Call API functions here to get data.
         const { currencies, error } = await getCurrencyTotals();
-        this.setState({ currencies });
+        this.setState({ currencies, error, searchedCurrencies: currencies, loading: false });
     }
 
-    public renderRows(): JSX.Element[] {
-        const { currencies } = this.state;
-        const rows: JSX.Element[] = [];
+    /**
+     * Handles the submit of the form.
+     * @param {React.FormEvent<HTMLFormElement>} e html form event.
+     * @returns {Promise<void>}
+     */
+    private async onSubmit(e: React.FormEvent<HTMLFormElement>): Promise<void> {
+        e.preventDefault();
+        if (this.state.loading) return;
+        const { currencies, search } = this.state;
 
-        return currencies.map((currency, index) => (
-            <tr key={`currency${index + 1}`}>
-                <td>{currency.name}</td>
-                <td>{currency.amount}</td>
-                <td>3</td>
-            </tr>
-        ));
+        this.setState({ loading: true });
+        const searchedCurrencies = currencies.filter((currency) => currency.name.toLowerCase().indexOf(search) !== -1);
+        this.setState({ searchedCurrencies, loading: false });
+    }
+
+    /**
+     * Handles the input change.
+     * @param {React.ChangeEvent<HTMLInputElement>} e html input event.
+     * @returns {void}
+     */
+    protected onChange(e: React.ChangeEvent<HTMLInputElement>): void {
+        this.setState({ search: e.target.value });
     }
 
     // Main Render Function
     public render(): JSX.Element {
-        const { currencies } = this.state;
+        const { searchedCurrencies, search, loading, error } = this.state;
         return (
             <div className="dashboard-page">
                 <h1>Terra - Totals by Currency</h1>
-                <CurrencyTable currencies={currencies} />
+                <form onSubmit={this.onSubmit} className="search-block">
+                    <input type="text" value={search} onChange={this.onChange} disabled={loading} />
+                    <button type="submit" aria-controls="submit" aria-label="submit" disabled={loading}>Search</button>
+                </form>
+                {error}
+                <CurrencyTable currencies={searchedCurrencies} />
             </div>
         );
     }
